e5d3891ace0aae3e5780cdf203d0d7b0a589c835
Redeemer40 <victorjonah199@gmail.com>
2020-07-02 17:09:23 +0100

    ...

--- src/pages/deposit.js
+++ src/pages/deposit.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Redirect, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./register.css";
 
 export default class Deposit extends Component {
